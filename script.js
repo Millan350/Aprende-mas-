d62f1336440a11eb1bdcfb675dd99bfd9be649de
Millan350 <angelmillan917@gmail.com>
2025-06-23 18:34:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtén todos los enlaces de anclaje dentro de la misma página
-    const enlaces = document.querySelectorAll('a[href^="#"]');
-
-    enlaces.forEach(function(enlace) {
-        enlace.addEventListener('click', function(event) {
-            // Prevenir la acción predeterminada del enlace
-            event.preventDefault();
-            
-            // Desplazar suavemente a la sección correspondiente
-            const destino = document.querySelector(this.getAttribute("href"));
-            destino.scrollIntoView({
-                behavior: "smooth"
-            });
-        });
-    });
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    document.getElementById("mensajeExito").classList.remove("hidden");
+    document.getElementById("contactForm").reset();
 });
